42998c84bb8c8307f5226c907d15ba4d26487492
André Fincato <work@andrefincato.info>
2022-09-28 14:33:56 +0200

    site: add config file
    - enhanced toolbar link dialog for panel
    - sitemap xml (wip?)

---
+++ site/config/config.php
@@ -0,0 +1,36 @@
+<?php 
+return [
+    'debug' => true,
+    'gearsdigital.enhanced-toolbar-link-dialog' => [
+        'title' => '{{ page.title }}',
+        'filter' => null,
+        'sort' => null,
+        'qualified' => false,
+        'translations' => []
+    ],
+    'routes' => [
+        [
+            'pattern' => 'sitemap.xml',
+            'action'  => function() {
+                // <https://getkirby.com/docs/cookbook/content/sitemap>
+
+                $pages = site()->index()->listed()->limit(500);
+
+                // fetch the pages to ignore from the config settings,
+                // if nothing is set, we ignore the error page
+                $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+                $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+                // return response with correct header type
+                return new Kirby\Cms\Response($content, 'application/xml');
+            }
+        ],
+        [
+            'pattern' => 'sitemap',
+            'action'  => function() {
+                return go('sitemap.xml', 301);
+            }
+        ]
+    ]
+];
