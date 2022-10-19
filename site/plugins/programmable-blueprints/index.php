9029c15667042356a93b2c607b243ba19aef8ce9
André Fincato <work@andrefincato.info>
2022-10-19 18:55:45 +0200

    /panel: rename user editor to contributor

--- site/plugins/programmable-blueprints/index.php
+++ site/plugins/programmable-blueprints/index.php
@@ -4,18 +4,18 @@ use Kirby\Cms\App as Kirby;
 
 Kirby::plugin('cookbook/programmable-blueprints', [
     'blueprints' => [
-        // 'site' => function () {
-        //     if (($user = kirby()->user()) && $user->isAdmin()) {
-        //         return Data::read(__DIR__ . '/blueprints/site.admin.yml');
-        //     } else {
-        //         return Data::read(__DIR__ . '/blueprints/site.editor.yml');
-        //     }
-        // },
+        'site' => function () {
+            if (($user = kirby()->user()) && $user->isAdmin()) {
+                return Data::read(__DIR__ . '/blueprints/site.admin.yml');
+            } else {
+                return Data::read(__DIR__ . '/blueprints/site.contributor.yml');
+            }
+        },
         'pages/article' => function () {
             if (($user = kirby()->user()) && $user->isAdmin()) {
                 return Data::read(__DIR__ . '/blueprints/pages/article.admin.yml');
             } else {
-                return Data::read(__DIR__ . '/blueprints/pages/article.editor.yml');
+                return Data::read(__DIR__ . '/blueprints/pages/article.contributor.yml');
             }
         },
     ]
