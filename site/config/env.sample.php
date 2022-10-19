0615a357ea7e19d375160ccd78a0317a2e882d3d
André Fincato <work@andrefincato.info>
2022-10-19 22:29:13 +0200

    api: add env.sample.php

---
+++ site/config/env.sample.php
@@ -0,0 +1,9 @@
+<?php
+// rename this file to => env.php
+// make a new user account with role API
+// pass email and password below
+
+return [
+    'api_user' => 'work@andrefincato.info',
+    'api_pass' => 'kirbykirby'
+];
