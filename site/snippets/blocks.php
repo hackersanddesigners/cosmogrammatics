d6f8289aeaa26b9533ebb246c7a748181c56aea2
André Fincato <work@andrefincato.info>
2022-10-27 18:22:37 +0200

    article: sketch out possible footnote setup (wip)

--- site/snippets/blocks.php
+++ site/snippets/blocks.php
@@ -1,5 +1,26 @@
+<?php $footnotes_all = []; ?>
+
 <?php foreach ($column->blocks() as $block): ?>
 <div id="<?= $block->id() ?>" data-type="block-<?= $block->type() ?>">
-  <?= $block ?>
+  <?php if ($block->type() === 'text'): ?>
+       <?= $block->text() ?>
+       <?php
+         $footnotes = $block->footnotes()->toStructure();
+         foreach($footnotes as $footnote) {
+             array_push($footnotes_all, $footnote->note());
+         };
+       ?>
+   <?php else: ?>
+       <?= $block ?>
+   <?php endif ?>
 </div>
 <?php endforeach ?>
+
+<div class="notes">
+<?php if($footnotes_all): ?>
+   <ol>
+<?php foreach($footnotes_all as $footnote): ?>
+     <li><?= $footnote ?><a href="">↩</a></li>
+     <?php endforeach ?>
+   </ol>
+<?php endif ?>
