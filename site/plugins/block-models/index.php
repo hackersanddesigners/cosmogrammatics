716bbca9cfa06e440fbac6ec946bfc8087938b58
André Fincato <work@andrefincato.info>
2022-11-01 20:41:11 +0100

    footnote plugin: working prototype

--- site/plugins/block-models/index.php
+++ site/plugins/block-models/index.php
@@ -1,33 +1,68 @@
 <?php
 
 use Kirby\Cms\Block;
+use Kirby\Cms\Content;
 
 class DefaultBlock extends Block
 {
     public function id(): string
     {
 
+        // check if block is of type `text`, else skip
         if ($this->type() === 'text') {
 
             // -- footnote refs
             // regex against `<article-footnote>` tag and replace it with:
             // - `{content <article-footnote>} <a href="<short-hash-id>">{counter ref}</a>`
 
-            $text = $this->content()->text();
+            // check if text->footnotes has any note
+            // if yes, start matching any `<article-footnote` found in
+            // $this->content->text
 
-            $pattern = '/\<article-footnote>(.*)<\/article-footnote>/m';
-            $replacement = '$1 <a href="#ref-' . $uuid . '" class="ref"><span>[*]</span></a>';
+            $footnotes_count = $this->content()->footnotes()->toStructure()->count();
 
-            $new_text = preg_replace($pattern, $replacement, $text);
+            if ($footnotes_count > 0) {
 
-            $content = $this->content()->toArray();
-            $content['text'] = $new_text;
-            $content_update = $this->content()->update($content, true);
+                $in = $this->content()->text();
+                $pattern = '/<article-footnote>(.*)<\/article-footnote>/mU';
 
-            return 'block-' . parent::id();
+                // <https://stackoverflow.com/a/11174818>
+                $out = preg_replace_callback(
+                    $pattern,
+                    function ($matches){
+                        static $ref = 0;
+                        $ref++;
+
+                        $ft_ref = 'ft-' . $ref;
+                        $ft_note = '#note-ref-' . $ref;
+
+                        $replacement = $matches[1] . '<a id="' . $ft_ref . '" href="' . $ft_note . '" class="ref-ft"><span>[' . $ref . ']</span></a>';
+
+                        return $replacement;
+                    },
+                    $in);
+
+
+                // var_dump($out);
+
+                // TODO: this replaces the actual text in the file
+                // does it make sense to instead replace the produced HTML
+                // used in the template? (maybe with caching?)
+                
+                // $content = $this->content()->toArray();
+                // $content['text'] = $out;
+                // $content_update = $this->content()->update($content, true);
+
+            }
+
+            return parent::id();
+
+            // return $this->content();
 
         } else {
-            return 'block-' . parent::id();
+            return parent::id();
+
+            // return $this->content();
         }
     }
 }
