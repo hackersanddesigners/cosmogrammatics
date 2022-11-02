a76e72c9e58c8aebd5ae159b84f1badcbb3321dc
André Fincato <work@andrefincato.info>
2022-11-02 20:01:40 +0100

    footnote-plugin: merge blockModel to footnote-ref-update plugin

--- site/plugins/footnote-ref-update/index.php
+++ site/plugins/footnote-ref-update/index.php
@@ -13,23 +13,101 @@ Kirby::plugin('cosmo/footnote-ref', [
             foreach($blocks as $block) {
                 if ($block->type() === 'text') {
 
-                    $footnotes = [];
-                    foreach($block->footnotes()->toStructure() as $footnote) {
+                    // -- footnote refs
+                    // regex against `<article-footnote>` tag and replace it with:
+                    // - `{content <article-footnote>} <a href="<short-hash-id>">{counter ref}</a>`
+                    // - in order to make the code work also on successive
+                    //   call, for instance when some new footnotes are being added
+                    //   later on — either on newest parts of the text or on previous
+                    //   ones, we do a second match_replace to update the numbering
+
+                    // check if text->footnotes has any note
+                    // if yes, start matching any `<article-footnote` found in
+                    // $block->text()
+
+                    $footnotes = $block->footnotes()->toStructure();
+                    $footnotes_count = $footnotes->count();
+
+                    if ($footnotes_count > 0) {
+
+                        $pattern_one_in = $block->text();
+                        $pattern_one = '/<article-footnote>(.*)<\/article-footnote>/mU';
+
+                        // <https://stackoverflow.com/a/11174818>
+                        $callback = function ($matches) use ($block) {
+                            // this ref works only the first time
+                            // if new footnotes are added afterwards
+                            // the regex (correctly) don't match on them
+                            // but only on the new footnotes, so
+                            // the count starts from 1 again
+
+                            static $ref = 0;
+                            $ref++;
+
+                            $ft_ref = 'ft-' . $block->id() . '-' . $ref;
+                            $ft_note = '#note-ref-' . $block->id() . '-' . $ref;
+
+                            $replacement = $matches[1] . '<a id="' . $ft_ref . '" href="' . $ft_note . '" class="ref-ft"><span>[' . $ref . ']</span></a>';
+
+                            return $replacement;
+                        };
+
+                        $pattern_one_text_new = preg_replace_callback($pattern_one, $callback, $pattern_one_in);
+
+                    }
+
+                    // -- second pass, bleah ):
+
+                    $pattern_two = '/\<a id="*" href="#note-ref(.*)" (.*)\>(.*)\<\/a>/mU';
+
+                    $callback = function ($matches) use ($block) {
+                        static $ref = 0;
+                        $ref++;
+
+                        $ft_ref = 'ft-' . $block->id() . '-' . $ref;
+
+                        $ft_note = '#note-ref-' . $block->id() . '-' . $ref;
+
+                        $replacement = '<a id="' . $ft_ref . '" href="' . $ft_note . '" class="ref-ft"><span>[' . $ref . ']</span></a>';
+
+                        return $replacement;
+                    };
+
+                    $text_new = preg_replace_callback($pattern_two, $callback, $pattern_one_text_new);
+
+                    // $xml = simplexml_load_string($pattern_one_text_new);
+                    // var_dump($xml);
+                    // $list = $xml->xpath("//@href");
+
+                    // $preparedUrls = array();
+                    // foreach($list as $item) {
+                    //     var_dump($item);
+                    //     $item = parse_url($item);
+                    //     $preparedUrls[] = $item['scheme'] . '://' .  $item['host'] . '/';
+                    // }
+
+
+                    // -- footnote notes
+                    $footnotes_new = [];
+                    foreach($footnotes as $footnote) {
                         // get footnote index to use for the `ref` field
-                        $ref = $footnote->indexOf($block->footnotes()->toStructure());
+                        $ref = $footnote->indexOf($footnotes);
 
                         $new_footnote = array(
                             'note' => $footnote->note()->value(),
                             'ref' => $ref +1,
                         );
 
-                        array_push($footnotes, $new_footnote);
+                        array_push($footnotes_new, $new_footnote);
                     }
 
+
+                    // -- update block
                     $blockUpdated = new Kirby\Cms\Block([
                         'content' => [
-                            'text' => $block->text()->value(),
-                            'footnotes' => $footnotes,
+                            // 'text' => $block->text()->value(),
+                            'text' => $text_new,
+                            'footnotes' => $footnotes_new,
                         ],
                         'type' => 'text',
                     ]);
