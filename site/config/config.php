0f696f37272862a5c877455db41787c3984c90c7
André Fincato <work@andrefincato.info>
2022-11-10 11:54:39 +0100

    comment-add: change auth method, update POST request and fields

--- site/config/config.php
+++ site/config/config.php
@@ -2,12 +2,6 @@
 
 return [
     'debug' => true,
-    'env' => require_once 'env.php',
-    'api' => [
-        'basicAuth' => true,
-        'allowInsecure' => true
-    ],
-
     'gearsdigital.enhanced-toolbar-link-dialog' => [
         'title' => '{{ page.title }}',
         'filter' => null,
