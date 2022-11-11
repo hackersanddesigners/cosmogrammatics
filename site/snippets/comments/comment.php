4a998667de305fc3be6b97b717c01e39c4469e39
André Fincato <work@andrefincato.info>
2022-11-11 19:52:54 +0100

    comments: save date in text field, adjust date print in template
    saving date in date field (with time option enabled) would still cut
    the second field, bleah.

--- site/snippets/comments/comment.php
+++ site/snippets/comments/comment.php
@@ -3,7 +3,7 @@
     <?= $comment->text()->html() ?>
   </section>
   <footer>
-    <p><time datetime="<?= $comment->timestamp()->toDate('Y-m-d H:m:s') ?>">On <?= $comment->timestamp()->toDate('Y-m-d H:m:s') ?></time><p>
+    <p><time datetime="<?= $comment->timestamp()->toDate('Y-m-d H:i:s') ?>">On <?= $comment->timestamp()->toDate('Y-m-d H:i:s') ?></time><p>
     <p>by <?= $comment->user()->html() ?></p>
     <!-- <a href="#<?= $comment->block_id() ?>">Show Block</a> -->
   </footer>
