650f9555ce82bfdd9da63d2734cb7ad0489eb067
André Fincato <work@andrefincato.info>
2022-11-11 16:19:11 +0100

    block-audio: update plugin structure to use Vue

--- site/plugins/block-audio/index.js
+++ site/plugins/block-audio/index.js
@@ -1,40 +1,89 @@
-panel.plugin('cosmo/block-audio', {
-  blocks: {
-    audio: {
-      computed: {
-        source() {
-          if (this.content.source_file[0]) {
-            return this.content.source_file[0]
-
-          } else if (this.content.source_embed[0]) {
-            return this.content.source_embed[0]
-
-          } else {
-            {}
-          }
-        }
-      },
-      template: `
-        <div>
-          <div v-if="source.url">
-            <h1>{{ content.title }}</h1>
-            <audio controls style="width: 100%">
-              <source :src="source.url" type="audio/mpeg">
-            </audio>
-          </div>
-          <div v-else>No audio selected</div>
-        </div>
-      `
+(function() {
+  "use strict";
+  function normalizeComponent(scriptExports, render, staticRenderFns, functionalTemplate, injectStyles, scopeId, moduleIdentifier, shadowMode) {
+    var options = typeof scriptExports === "function" ? scriptExports.options : scriptExports;
+    if (render) {
+      options.render = render;
+      options.staticRenderFns = staticRenderFns;
+      options._compiled = true;
     }
+    if (functionalTemplate) {
+      options.functional = true;
+    }
+    if (scopeId) {
+      options._scopeId = "data-v-" + scopeId;
+    }
+    var hook;
+    if (moduleIdentifier) {
+      hook = function(context) {
+        context = context || this.$vnode && this.$vnode.ssrContext || this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext;
+        if (!context && typeof __VUE_SSR_CONTEXT__ !== "undefined") {
+          context = __VUE_SSR_CONTEXT__;
+        }
+        if (injectStyles) {
+          injectStyles.call(this, context);
+        }
+        if (context && context._registeredComponents) {
+          context._registeredComponents.add(moduleIdentifier);
+        }
+      };
+      options._ssrRegister = hook;
+    } else if (injectStyles) {
+      hook = shadowMode ? function() {
+        injectStyles.call(
+          this,
+          (options.functional ? this.parent : this).$root.$options.shadowRoot
+        );
+      } : injectStyles;
+    }
+    if (hook) {
+      if (options.functional) {
+        options._injectStyles = hook;
+        var originalRender = options.render;
+        options.render = function renderWithStyleInjection(h, context) {
+          hook.call(context);
+          return originalRender(h, context);
+        };
+      } else {
+        var existing = options.beforeCreate;
+        options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+      }
+    }
+    return {
+      exports: scriptExports,
+      options
+    };
   }
-});
-
-  // <div v-if="source_file.url">
-  // <h3>{{ content.title }}</h3>
-  // <audio controls>
-  // <source :src="content.source_file.url" type="audio/mpeg">
-  // </audio>
-  // </div>
-  // <div v-elseif="source_embed.url">
-  // <p>{{ content.source_embed.url }}</p>
-  // <div v-else>No audio added</div>
+  const _sfc_main = {
+    computed: {
+      source() {
+        if (this.content.source_file[0]) {
+          return this.content.source_file[0];
+        }
+      }
+    }
+  };
+  var _sfc_render = function render() {
+    var _vm = this, _c = _vm._self._c;
+    return _c("div", [_vm.source.url ? _c("div", [_c("h1", [_vm._v(_vm._s(_vm.content.title))]), _c("audio", { staticStyle: { "width": "100%" }, attrs: { "controls": "" } }, [_c("source", { attrs: { "src": _vm.source.url, "type": "audio/mpeg" } })])]) : _c("div", [_vm._v("No audio selected")])]);
+  };
+  var _sfc_staticRenderFns = [];
+  _sfc_render._withStripped = true;
+  var __component__ = /* @__PURE__ */ normalizeComponent(
+    _sfc_main,
+    _sfc_render,
+    _sfc_staticRenderFns,
+    false,
+    null,
+    null,
+    null,
+    null
+  );
+  __component__.options.__file = "/Users/af-etc/Projects/cosmogrammatics/www/site/plugins/block-audio/src/components/Audio.vue";
+  const Audio = __component__.exports;
+  panel.plugin("cosmo/block-audio", {
+    blocks: {
+      audio: Audio
+    }
+  });
+})();
---
+++ site/plugins/block-audio/src/components/Audio.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="source.url">
+      <h1>{{ content.title }}</h1>
+      <audio controls style="width: 100%">
+        <source :src="source.url" type="audio/mpeg">
+      </audio>
+    </div>
+    <div v-else>No audio selected</div>
+  </div>
+</template>
+
+<script>
+export default {
+   computed: {
+     source() {
+       if (this.content.source_file[0]) {
+         return this.content.source_file[0]
+       } else {
+         {}
+       }
+     }
+   }
+};
+</script>
