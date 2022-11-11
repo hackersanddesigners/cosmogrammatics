9e98c9a11ceb6b8a178b3d337d4d733dda78eb7d
André Fincato <work@andrefincato.info>
2022-11-11 12:06:21 +0100

    block-audio: add embed URL option beside file upload

--- site/plugins/block-audio/snippets/blocks/audio.php
+++ site/plugins/block-audio/snippets/blocks/audio.php
@@ -1,11 +1,20 @@
-<?php if ($file = $block->source()->toFile()): ?>
+<?php if ($file = $block->source_file()->toFile()): ?>
 <div class="audio-wrapper">
   <audio controls>
      <source src="<?= $file->url() ?>" type="<?= $file->mime() ?>">
     Your browser does not support the <code>audio</code> element.
   </audio> 
   <div class="audio-info">
-  <h3><?= $block->title()->html() ?></h3>
-  <div><?= $block->caption()->html() ?></div>
+    <h3><?= $block->title()->html() ?></h3>
+    <div><?= $block->caption()->html() ?></div>
+  </div>
+</div>
+<?php elseif ($url = $block->source_url()): ?>
+<div class="audio-wrapper">
+  <div class="iframe-wrapper"><?= $url ?></div>
+  <div class="audio-info">
+    <h3><?= $block->title()->html() ?></h3>
+    <div><?= $block->caption()->html() ?></div>
+  </div>
 </div>
 <?php endif ?>
