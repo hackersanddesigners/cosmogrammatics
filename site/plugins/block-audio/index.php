622c74c178fb2a3471d4c6a2c4ee5aa104f921db
André Fincato <work@andrefincato.info>
2022-11-11 11:55:44 +0100

    custom-block-audio: init

---
+++ site/plugins/block-audio/index.php
@@ -0,0 +1,12 @@
+<?php
+
+Kirby::plugin('cosmo/block-audio', [
+    'blueprints' => [
+        'blocks/audio' => __DIR__ . '/blueprints/blocks/audio.yml',
+        'files/audio' => __DIR__ . '/blueprints/files/audio.yml',
+        'files/poster' => __DIR__ . '/blueprints/files/poster.yml',
+    ],
+    'snippets' => [
+        'blocks/audio' => __DIR__ . '/snippets/blocks/audio.php'
+    ],
+]);
