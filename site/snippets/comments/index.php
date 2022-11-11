261742fdb56428be34f1107e6063b86974316932
كارل مبارك <bonjour@moubarak.eu>
2022-11-11 16:00:55 +0100

    started in per-block comment in snippets and css

--- site/snippets/comments/index.php
+++ site/snippets/comments/index.php
@@ -1,14 +1,19 @@
 <?php if ($comments && $comments->count() > 0): ?>
   <aside>
-    <?php
-      foreach ($comments as $comment) {
-        snippet( 'comments/comment', [ 'comment' => $comment ] );
-      }
-      snippet( 'comments/add', [
-        'block'          => $block,
-        'selection_type' => $comments->first()->selection_type(),
-        'selection_text' => $comments->first()->selection_text()
-      ])
-    ?>
+    <?php foreach( $threads as $thread ): ?>
+      <?= var_dump($thread) ?>
+      <section class="thread">
+        <?php
+          foreach ($thread->$comments as $comment) {
+            snippet( 'comments/comment', [ 'comment' => $comment ] );
+          }
+         snippet( 'comments/add', [
+            'block'          => $block,
+            'selection_type' => $comments->first()->selection_type(),
+            'selection_text' => $comments->first()->selection_text()
+          ])
+        ?>
+      </section>
+    <?php endforeach ?>
   </aside>
 <?php endif ?>
--- site/snippets/grid/block.php
+++ site/snippets/grid/block.php
@@ -5,27 +5,29 @@
     data-type="block-<?= $block->type() ?>"
 >
 
-<div class="contents">
-<?php
-    if ( $block->layout()->isNotEmpty() ) {
-        snippet( 'grid/index', [ 'rows' => $block->layout()->toLayouts() ] );
-    } else {
-        echo $block;
-    } ?>
-</div>
+
+  <div class="contents">
+  <?php
+      if ( $block->layout()->isNotEmpty() ) {
+          snippet( 'grid/index', [ 'rows' => $block->layout()->toLayouts() ] );
+      } else {
+          echo $block;
+      } ?>
+  </div>
+
 
 <?php
 
     // <2022-11-10T20:55> andré: don't know how to put this inside a
     // controller for blocks, not sure there's such a thing yet...
-    // on the upside, not having to do nested loops is good! 
+    // on the upside, not having to do nested loops is good!
     $block_comments = $comments->filterBy('block_id', $block->bid());
 
     $block_threads = [];
     if ($comments) {
-    
+
         $block_comments = $comments->filterBy('block_id', $block->bid());
-    
+
         // foreach ($block_comments as $comment) {
         //     $selection_type = $comment->selection_type();
         //     // magic trick to call $comment->selection{text, image, audio, video}()
@@ -34,23 +36,23 @@
         //     $selection_coords = $comment->{ $selection_coords_key }();
         //     $selection_coords_data = $selection_coords->toStructure();
         // }
-    
+
         // for each block comment selection, check how many
         // comments there are
         foreach($block_comments as $block_comment) {
-    
+
             $selection_type = $block_comment->selection_type();
             $selection_coord = $block_comment->selection_coords()->first()->value();
-    
+
             // testing with selection_text for now
-            // while figuring out decent way to filter out 
+            // while figuring out decent way to filter out
             $matches = $block_comments
                      ->filterBy('selection_type', 'text')
                      ->filterBy('selection_coords', '!=', 'NULL')
                      ->filterBy('selection_coords', '==', $selection_coord);
-    
+
             if ($matches->count() > 0) {
-    
+
                 $thread = [
                     'selection' => [
                         'type'   => $selection_type,
@@ -58,10 +60,10 @@
                     ],
                     'comments' => $matches
                 ];
-    
+
                 array_push($block_threads, $thread);
             }
-        }; 
+        };
     }
 
     snippet( 'comments/index', [
@@ -70,4 +72,7 @@
         'threads'  => $block_threads,
     ] )
 ?>
+
+<?= snippet( 'toolbar/index', [ 'block' => $block ] ) ?>
+
 </section>
