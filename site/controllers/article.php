a678dc61e23042dd14306bf41bdf417d413eb5bc
كارل مبارك <bonjour@moubarak.eu>
2022-11-14 15:54:23 +0100

    moved get_block_threads functioon to controller, but calling it in template

--- site/controllers/article.php
+++ site/controllers/article.php
@@ -1,5 +1,63 @@
 <?php
 
+
+// ANDRÉ I am defining this function here, but only able to use it
+// in the block.php snipppet itself.
+
+function get_block_threads( $block, $comments ) {
+
+  $threads = [];
+
+  // get all comments related to this block
+
+  if ( $comments ) {
+    $block_comments = $comments->filterBy('block_id', $block->bid());
+
+    // for each block_comment check if there are comments
+    // that belong to the same "thread", meaning they share
+    // a selection type and coords
+
+    foreach( $block_comments as $block_comment ) {
+
+      $selection_type   = $block_comment->selection_type();
+      $selection_coords = $block_comment->selection_coords();
+
+      // first, check if thread has already been created.
+      // It can be uniquely identified by its selection type
+      // and coords.
+
+      $thread_exists = FALSE;
+      foreach ( $threads as $thr ) {
+        if (
+          $thr['selection_type']->value() == $selection_type->value() &&
+          $thr['selection_coords']->value() == $selection_coords->value()
+        ) {
+          $thread_exists = TRUE;
+          break;
+        }
+      }
+
+      // if the thread doesn't exist, we proceed with it's
+      // creation and addition to the $threads array.
+
+      if ( !$thread_exists ) {
+        $threads[] = [
+          'selection_type'   => $selection_type,
+          'selection_coords' => $selection_coords,
+          'comments'         => $block_comments
+            ->filterBy('selection_type', $selection_type )
+            ->filterBy('selection_coords', $selection_coords )
+        ];
+      }
+
+    };
+  }
+
+  return $threads;
+
+}
+
+
 return function ($page) {
 
     // <article>/comments/<[comment-1, comment-2, ...]>
@@ -37,7 +95,8 @@ return function ($page) {
         }
     }
 
-    
+    // doesnt work here for some reason
+    // $block->thread = get_block_threads( $block, $comments )
 
     $skin = [
         'colors' => $page->colors()->toEntity(),
--- site/snippets/grid/block.php
+++ site/snippets/grid/block.php
@@ -5,7 +5,6 @@
     data-type="block-<?= $block->type() ?>"
 >
 
-
   <div class="contents">
     <?php if ( $block->layout()->isNotEmpty() ) {
       snippet( 'grid/index', [ 'rows' => $block->layout()->toLayouts() ] );
@@ -14,69 +13,9 @@
     } ?>
   </div>
 
-
-  <?php
-
-    // <2022-11-10T20:55> andré: don't know how to put this inside a
-    // controller for blocks, not sure there's such a thing yet...
-    // on the upside, not having to do nested loops is good!
-
-    $block_comments = [];
-    $block_threads  = [];
-
-    if ( $comments ) {
-
-      // get all comments related to this block
-
-      $block_comments = $comments->filterBy('block_id', $block->bid());
-
-      // for each block_comment check if there are comments
-      // that belong to the same "thread", meaning they share
-      // a selection type and coords
-
-      foreach( $block_comments as $block_comment ) {
-
-        $selection_type   = $block_comment->selection_type();
-        $selection_coords = $block_comment->selection_coords();
-
-        // first, check if thread has already been created.
-        // It can be uniquely identified by its selection type
-        // and coords.
-
-        $thread_exists = FALSE;
-        foreach ( $block_threads as $thr ) {
-          if (
-            $thr['selection_type']->value() == $selection_type->value() &&
-            $thr['selection_coords']->value() == $selection_coords->value()
-          ) {
-            $thread_exists = TRUE;
-            break;
-          }
-        }
-
-        // if the thread doesn't exist, we proceed with it's
-        // creation and addition to the $block_threads array.
-
-        if ( !$thread_exists ) {
-          $block_threads[] = [
-            'selection_type'   => $selection_type,
-            'selection_coords' => $selection_coords,
-            'comments'         => $block_comments
-              ->filterBy('selection_type', $selection_type )
-              ->filterBy('selection_coords', $selection_coords )
-          ];
-        }
-
-      };
-    }
-
-    snippet( 'comments/index', [
-        'block'    => $block,
-        'comments' => $block_comments,
-        'threads'  => $block_threads,
-    ] )
-
-
-  ?>
+  <?php snippet( 'comments/index', [
+    'block'   => $block,
+    'threads' => get_block_threads( $block, $comments )
+  ]) ?>
 
 </section>
