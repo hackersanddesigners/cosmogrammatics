fe3bdd1209eab8ae095b771473e64c84f9aa775b
كارل مبارك <bonjour@moubarak.eu>
2022-11-18 14:05:40 +0100

    css for image, video, audio embeds

--- site/plugins/block-video/snippets/blocks/video.php
+++ site/plugins/block-video/snippets/blocks/video.php
@@ -1,12 +1,14 @@
 <?php if ($file = $block->source_file()->toFile()): ?>
-<div class="video-wrapper">
-  <video controls>
-     <source src="<?= $file->url() ?>" type="<?= $file->mime() ?>">
+  <video
+    controls
+    preload="metadata"
+  >
+     <source
+      src="<?= $file->url() ?>"
+      type="<?= $file->mime() ?>"
+    >
     Your browser does not support the <code>video</code> element.
-  </video> 
-  <div class="video-info">
-    <h3><?= $block->title()->html() ?></h3>
-    <div><?= $block->caption()->html() ?></div>
-  </div>
-</div>
+  </video>
+  <h3><?= $block->title() ?></h3>
+  <div class="caption"><?= $block->caption() ?></div>
 <?php endif ?>
