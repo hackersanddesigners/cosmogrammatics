166a6e43a76d3fc3cf94528a9d0fff165d03f8e1
André Fincato <work@andrefincato.info>
2022-12-14 22:47:26 +0100

    article-comments: fetch all comments from backend and merge w/ localStorage

---
+++ assets/js/fetch-article-highlights-from-api.js
@@ -0,0 +1,25 @@
+const textHighlightMergeUnique = require('./text-highlights-merge-unique')
+
+async function fetchArticleHighlightsFromAPI(article_slug) {
+  const csrf = document.querySelector('.toolbar > form').getAttribute('data-csrf')
+  
+  const url = `/api/cosmo/${ article_slug }`
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "X-CSRF" : csrf
+    }
+  })
+  .then(response => response.json())
+  .then(response => {
+    const newData = response
+    textHighlightMergeUnique(newData)
+
+  })
+  .catch(error => {
+    console.log('fetchArticleHighlightsFromAPI =>', error)
+  });
+
+}
+
+module.exports = fetchArticleHighlightsFromAPI
--- assets/js/templates/article.js
+++ assets/js/templates/article.js
@@ -1,10 +1,18 @@
-const textHighlight = require('../text-highlight.js')
-const { respond_comment, post_comment } = require('../comments.js')
+const fetchArticleHighlightsFromAPI = require('../fetch-article-highlights-from-API')
+const textHighlight = require('../text-highlight')
+const { respond_comment, post_comment } = require('../comments')
 
 const comment_forms = document.querySelectorAll( '.comment_form' )
 for ( const comment_form of comment_forms ) {
   comment_form.onsubmit = respond_comment
 }
+
+// synchronize text-highlights backend data with localStorage
+const article_slug = window.location.pathname.split('/').pop().split('/').join('+')
+
+;(async() => {
+  await fetchArticleHighlightsFromAPI(article_slug)
+})()
 
 const article_element    = document.querySelector( 'main' )
 const selection_toolbar  = document.querySelector( '.toolbar' )
