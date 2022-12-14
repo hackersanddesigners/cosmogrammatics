fd7fb5c65282c6384f614c632a5c0021c0943864
André Fincato <work@andrefincato.info>
2022-12-14 22:47:04 +0100

    article-comments plugin: init

---
+++ site/plugins/article-comments/index.php
@@ -0,0 +1,45 @@
+<?php
+
+Kirby::plugin('cosmo/article-comments', [
+    'api' => [
+        'routes' => function ($kirby) {
+            return [
+                [
+                    'pattern' => 'cosmo/(:alphanum)',
+                    'action' => function ($alphanum) use ($kirby) {
+
+                        $article_path = 'articles/' . $alphanum . '/comments';
+                        $comments = $kirby->page($article_path)->children()->listed();
+
+                        $text_highlights = [];
+                        foreach($comments as $comment) {
+                            $selection_text = $comment->selection_text()->toObject();
+                            $endMeta = $selection_text->endMeta()->toObject();
+                            $startMeta = $selection_text->startMeta()->toObject();
+
+                            $highlight = [
+                                '__isHighlightSource' => $selection_text->__isHighlightSource()->value(),
+                                'endMeta' => [
+                                    'parentIndex' => $endMeta->parentIndex()->value(),
+                                    'parentTagName' => $endMeta->parentTagName()->value(),
+                                    'textOffset' => $endMeta->textOffset()->value(),
+                                ],
+                                'id' => $selection_text->id()->value(),
+                                'startMeta' => [
+                                    'parentIndex' => $startMeta->parentIndex()->value(),
+                                    'parentTagName' => $startMeta->parentTagName()->value(),
+                                    'textOffset' => $startMeta->textOffset()->value(),
+                                ],
+                                'text' => $selection_text->text()->value()
+                            ];
+
+                            array_push($text_highlights, $highlight);
+                        };
+
+                        return json_encode($text_highlights);
+                    }
+                ]
+            ];
+        }
+    ]
+]);
