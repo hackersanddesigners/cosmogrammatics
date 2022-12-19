137a4d732da7c21ecbb24347114a302862766343
André Fincato <work@andrefincato.info>
2022-12-19 14:58:48 +0100

    config: add env.sample

---
+++ site/config/env.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// create a new user with role: API in the panel
+// set user's email and password below
+// rename this file to `env.php`
+
+return [
+    'api_email' => '',
+    'api_pass' => '',
+];
