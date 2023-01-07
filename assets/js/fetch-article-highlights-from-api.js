830e2b1cefecbfea7178c7e24573f303c1fbe377
André Fincato <work@andrefincato.info>
2023-01-07 17:06:26 +0100

    article-comments: remove API auth layer
    published comments are public. towards simplifying kirby auth setup
    w/o using JS to do POST operation when commenting, but going through
    PHP.
    
    having a comment-review step allows to hit the post comment button and
    refresh the page without producing any annoying (i think) user
    experience flow "destruction".

--- assets/js/fetch-article-highlights-from-api.js
+++ assets/js/fetch-article-highlights-from-api.js
@@ -1,21 +1,15 @@
 const textHighlightMergeUnique = require('./text-highlights-merge-unique')
 
 async function fetchArticleHighlightsFromAPI(article_slug) {
-  const csrf = document.querySelector('.toolbar > form').getAttribute('data-csrf')
- 
-  const url = `/api/cosmo/${ article_slug }`
-  return fetch(url, {
-    method: "GET",
-    headers: {
-      "X-CSRF" : csrf
-    }
-  })
-  .then(response => response.json())
-  .then(response => {
-    const newData = response
-    textHighlightMergeUnique(newData)
+  const url = `/cosmo-api/${ article_slug }`
 
-  })
+  return fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      const newData = response
+      textHighlightMergeUnique(newData)
+
+    })
   .catch(error => {
     return []
   });
--- site/plugins/article-comments/index.php
+++ site/plugins/article-comments/index.php
@@ -1,45 +1,43 @@
 <?php
 
-Kirby::plugin('cosmo/article-comments', [
-    'api' => [
-        'routes' => function ($kirby) {
-            return [
-                [
-                    'pattern' => 'cosmo/(:any)',
-                    'action' => function ($alphanum) use ($kirby) {
+Kirby::plugin('cosmo-api/article-comments', [
+    'routes' => function ($kirby) {
+        return [
+            [
+                'pattern' => 'cosmo-api/(:any)',
+                'action' => function ($alphanum) use ($kirby) {
 
-                        $article_path = 'articles/' . $alphanum . '/comments';
-                        $comments = $kirby->page($article_path)->children()->listed();
+                    $article_path = 'articles/' . $alphanum . '/comments';
+                    $comments = $kirby->page($article_path)->children()->listed();
 
-                        $text_highlights = [];
-                        foreach($comments as $comment) {
-                            $selection_text = $comment->selection_text()->toObject();
-                            $endMeta = $selection_text->endMeta()->toObject();
-                            $startMeta = $selection_text->startMeta()->toObject();
+                    $text_highlights = [];
+                    foreach($comments as $comment) {
+                        $selection_text = $comment->selection_text()->toObject();
+                        $endMeta = $selection_text->endMeta()->toObject();
+                        $startMeta = $selection_text->startMeta()->toObject();
 
-                            $highlight = [
-                                '__isHighlightSource' => $selection_text->__isHighlightSource()->value(),
-                                'endMeta' => [
-                                    'parentIndex' => $endMeta->parentIndex()->value(),
-                                    'parentTagName' => $endMeta->parentTagName()->value(),
-                                    'textOffset' => $endMeta->textOffset()->value(),
-                                ],
-                                'id' => $selection_text->id()->value(),
-                                'startMeta' => [
-                                    'parentIndex' => $startMeta->parentIndex()->value(),
-                                    'parentTagName' => $startMeta->parentTagName()->value(),
-                                    'textOffset' => $startMeta->textOffset()->value(),
-                                ],
-                                'text' => $selection_text->text()->value()
-                            ];
+                        $highlight = [
+                            '__isHighlightSource' => $selection_text->__isHighlightSource()->value(),
+                            'endMeta' => [
+                                'parentIndex' => $endMeta->parentIndex()->value(),
+                                'parentTagName' => $endMeta->parentTagName()->value(),
+                                'textOffset' => $endMeta->textOffset()->value(),
+                            ],
+                            'id' => $selection_text->id()->value(),
+                            'startMeta' => [
+                                'parentIndex' => $startMeta->parentIndex()->value(),
+                                'parentTagName' => $startMeta->parentTagName()->value(),
+                                'textOffset' => $startMeta->textOffset()->value(),
+                            ],
+                            'text' => $selection_text->text()->value()
+                        ];
 
-                            array_push($text_highlights, $highlight);
-                        };
+                        array_push($text_highlights, $highlight);
+                    };
 
-                        return json_encode($text_highlights);
-                    }
-                ]
-            ];
-        }
-    ]
+                    return json_encode($text_highlights);
+                }
+            ]
+        ];
+    }
 ]);
