5b490cf2fa80f2c346f6e88e6f97d467feb3605f
André Fincato <work@andrefincato.info>
2023-01-13 13:16:30 +0100

    comment-review: add option to display connected text-highlight span

--- assets/js/comment-review.js
+++ assets/js/comment-review.js
@@ -143,6 +143,29 @@ function make_comment_el(comment, idx, article_slug) {
     commentReviewList(article_slug)      
   })
 
+  // -- add link to text-selection span
+  const highlight_id = comment.content.selection_text.id
+  const target = document.querySelector(`[data-highlight-id="${highlight_id}"]`)
+  target.setAttribute('id', highlight_id) 
+
+  const show_text_selection = document.createElement('a')
+  show_text_selection.setAttribute('href', `#${highlight_id}`)
+  show_text_selection.innerHTML = 'Show Text Selection'
+  show_text_selection.classList.add('comment-list-show-highlight')
+
+  show_text_selection.addEventListener('click', (e) => {
+    // set border around text-selection for a
+    // specific amount of time, then reset the style
+
+    target.style.border = '1px solid blue'
+
+    setTimeout(() => {
+      target.style.border = ''
+    }, '3000')
+  })
+
+  // --
+
   const comment_label_wrapper = document.createElement('div')
   comment_label_wrapper.append(comment_text)
   comment_label_wrapper.append(comment_date)
