7ede2fb53a0669a043c5d1188085660f32f49587
André Fincato <work@andrefincato.info>
2023-01-18 15:17:42 +0100

    text-highlight-merge-unique: fix de-duplication func

--- assets/js/text-highlights-merge-unique.js
+++ assets/js/text-highlights-merge-unique.js
@@ -1,22 +1,26 @@
 const LocalStore = require('./local.store')
 
 function textHighlightMergeUnique(newData, article_slug) {
-  // merge this array of text-highlights
-  // w/ current localStorage['combo-highlight']
+  // merge this array of text-highlights (newData)
+  // w/ current localStorage['comment-{article-slug}']
+
+  // when removing comment from backend only, we need force
+  // remove it from frontend as well: how to?
+
   const comment_store = new LocalStore(`comment-${article_slug}`);
   let comments = comment_store.getAll();
 
-  // <https://stackoverflow.com/a/54134237>
-  const ids = new Set(comments.map(d => d.id));
-  const highlightsMerged = [...comments, ...newData].filter(d => !ids.has(d.ID));
+  const highlightsMerged = [...comments, ...newData]
+  const highlights = highlightsMerged.filter((value, index, self) => {
+    return index === self.findIndex(t => {
+      return t.id === value.id
+    })
+  })
 
   // the save function checks if the item
   // exists already, so we just pass
   // the entire dataset
-  comment_store.save(highlightsMerged)
-
-  console.log('after-save =>', highlightsMerged, comment_store.getAll())
-
+  comment_store.save(highlights)
 }
 
 module.exports = textHighlightMergeUnique
