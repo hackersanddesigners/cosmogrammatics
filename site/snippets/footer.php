6fa714f6a476a594a48dbbcbacbe8d8782358414
André Fincato <work@andrefincato.info>
2023-01-18 15:30:17 +0100

    cache-boost js assets

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 <footer>
   <?php if ($page->intendedTemplate() == 'article') {
-    echo js([ 'assets/js/templates/article.min.js' ], [ 'type'  => 'module', 'async' => true ]);
+    echo js([ 'assets/js/templates/article.min.js?v=7ede2fb' ], [ 'type'  => 'module', 'async' => true ]);
   } ?>
 
   <p class="copyright"><?= $site->copyright()->html() ?></p>
