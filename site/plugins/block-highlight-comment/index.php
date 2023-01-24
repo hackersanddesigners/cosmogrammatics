7dc3bc292a86f80779bd67088ff00f7bff785754
André Fincato <work@andrefincato.info>
2023-01-24 21:10:22 +0100

    block-highlight-comment: remove unused code

--- site/plugins/block-highlight-comment/index.php
+++ site/plugins/block-highlight-comment/index.php
@@ -1,202 +1,22 @@
 <?php
 
-use Kirby\Cms\Page;
-use Kirby\Cms\Collection;
-use Kirby\Exception\PermissionException;
+// use Kirby\Cms\Page;
+// use Kirby\Cms\Collection;
+// use Kirby\Exception\PermissionException;
 
 
-
-// -- utils
-function adjust_offset ($n, $x) {
-    $x_container = ((2 + $n) + (3 + $n));
-    return $x_container + $x;
-};
-
-function wrapSelectedText ($text_in, $comment_offsets, $id) {
-    // split text using given offset from text-selection
-    // and wrap this text selection around an extra pair of tags
-    // (for now <span>{}</span>)
-    // => TODO check if wrap operation is done already
-
-    // each new item in the offset array works on a new `<t>` tag
-
-    $text = str_replace('</p>', '', $text_in);
-    $p_list = Str::split($text, '<p>');
-    // dump($p_list);
-
-    foreach($comment_offsets as $key => $value) {
-        // dump([$key, $value]);
-        // $left_side = str_slice($text_in, 0, $value['x1']);
-        // $center_side = str_slice($text_in, $value['x1'], $value['x2']);
-        // $right_side = str_slice($text_in, $value['x2']);
-
-        // $wrap_before = '<span class="comment-highlight" id="' . $id .'" >';
-        // $wrap_after = '</span>';
-
-        // // check if wrapping is done already
-        // if (Str::startsWith($left_side, $wrap_before)
-        //     OR Str::startsWith($right_side, $wrap_after)) {
-        //         return $text_in;
-        // };
-        // $text_out = $text_in;
-
-        // $text_out = $left_side . $wrap_before . $center_side . $wrap_after . $right_side;
-
-        // dump([$left_side, $center_side, $right_side]);
-
-    }
-
-    return $text_in;
-
-}
-
-// <https://stackoverflow.com/a/53985015>
-function str_slice() {
-    $args = func_get_args();
-
-    switch (count($args)) {
-    case 1:
-        return $args[0];
-    case 2:
-        $str        = $args[0];
-        $str_length = strlen($str);
-        $start      = $args[1];
-        if ($start < 0) {
-            if ($start >= - $str_length) {
-                $start = $str_length - abs($start);
-            } else {
-                $start = 0;
-            }
-        }
-        else if ($start >= $str_length) {
-            $start = $str_length;
-        }
-        $length = $str_length - $start;
-        return substr($str, $start, $length);
-    case 3:
-        $str        = $args[0];
-        $str_length = strlen($str);
-        $start      = $args[1];
-        $end        = $args[2];
-        if ($start >= $str_length) {
-            return "";
-        }
-        if ($start < 0) {
-            if ($start < - $str_length) {
-                $start = 0;
-            } else {
-                $start = $str_length - abs($start);
-            }
-        }
-        if ($end <= $start) {
-            return "";
-        }
-        if ($end > $str_length) {
-            $end = $str_length;
-        }
-        $length = $end - $start;
-        return substr($str, $start, $length);
-    }
-    return null;
-};
-
-
-function parseBlockSelection($blocks, $block_bid, $comments, $offset, $type) {
-
-    $updatedBlocks = [];
-
-    foreach($blocks as $block) {
-
-        $blockType = $block->type();
-
-        if ($blockType === 'columns') {
-
-            // we have one layout per block, no need to loop over
-            if ($layout = $block->layout()->toLayouts()->first()) {
-
-                $columnsNew = [];
-                foreach($layout->columns() as $column) {
-                    // we need to:
-                    // - parse the layout blocks
-                    // - reconstruct the layout with updated blocks
-                    // - convert it back to a layout object
-
-                    $subblocks = $column->blocks();
-                    $updatedSubblocks = parseBlockSelection($subblocks, $block_bid, $comments, $offset, 'layout');
-                    $subblocksNew = new Kirby\Cms\Blocks($updatedSubblocks);
-
-                    $columnNew = new Kirby\Cms\LayoutColumn(
-                        [
-                            'blocks' => $subblocksNew->toArray(),
-                            'width' => $column->width(),
-                        ]
-                    );
-
-                    array_push($columnsNew, $columnNew);
-                };
-
-                $layoutColumnsNew = new Kirby\Cms\LayoutColumns($columnsNew);
-
-                $layoutNew = Kirby\Cms\Layout::factory([
-                    'columns' => $layoutColumnsNew->toArray(),
-                ]);
-
-                $layoutsNew = new Kirby\Cms\Layouts([$layoutNew]);
-
-                // -- update block
-                $blockLayoutUpdated = [
-                    'content' => [
-                        'layout' => $layoutsNew->toArray(),
-                    ],
-                    'type' => 'columns',
-                ];
-
-                $blockLayoutNew = new Kirby\Cms\Block($blockLayoutUpdated);
-                array_push($updatedBlocks, $blockLayoutNew);
-            }
-
-        } else if ($blockType === 'text' && $block->bid() === $block_bid) {
-
-            $text_in = $block->text()->value();
-            $text_new = wrapSelectedText($text_in, $offset);
-
-            // -- update block
-            $blockUpdated = [
-                'content' => [
-                    'text' => $text_new,
-                    'footnotes' => $block->footnotes()->toArray(),
-                ],
-                'type' => $block->type(),
-            ];
-
-            $blockUpdated = new Kirby\Cms\Block($blockUpdated);
-            array_push($updatedBlocks, $blockUpdated);
-
-        } else {
-            array_push($updatedBlocks, $block);
-        }
-
-    }; // -- end blocks foreach
-
-
-    return $updatedBlocks;
-
-}
-
-
-// -- manipulate block content to add extra spans to highlight
-//    specified content in the offset
+// -- setup custom thread method
 Kirby::plugin('cosmo/block-highlight-comment', [
+
     'blockMethods' => [
-        // setup custom thread method
         // TODO add more commentary
-        'threads' => function ( $comments = [] ) {
+        'threads' => function ($comments = []) {
 
             $threads = [];
 
             // get all comments related to this block
 
-            if ( $comments ) {
+            if ($comments) {
                 $block_comments = $comments->filterBy('block_id', $this->bid() );
 
                 // for each block_comment check if there are comments
@@ -223,12 +43,9 @@ Kirby::plugin('cosmo/block-highlight-comment', [
                     $coord_id = join("_", $coord_label);
                     $id = $this->bid() . '_sel_' . $type . '-' . $coord_id;
 
-                    // dump(['id', $id]);
-
                     // first, check if thread has already been created.
                     // It can be uniquely identified by its selection type
                     // and coords.
-
                     $exists = FALSE;
                     foreach ( $threads as $thr ) {
                         if (
@@ -242,7 +59,6 @@ Kirby::plugin('cosmo/block-highlight-comment', [
 
                     // if the thread doesn't exist, we proceed with it's
                     // creation and addition to the $threads array.
-
                     if ( !$exists ) {
                         $threads[] = [
                             'selection_type'   => $type,
@@ -339,10 +155,7 @@ Kirby::plugin('cosmo/block-highlight-comment', [
             // $text_in = $updated->text();
 
 
-            $text_updated = wrapSelectedText( $text_in->toString(), $comment_offsets, '' );
-
             // dump($comment_offsets);
-
             return $updated;
         }
 
