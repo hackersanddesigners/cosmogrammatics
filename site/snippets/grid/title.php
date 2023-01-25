50ce18efdc7ae2c7f85d50c3da0a07221ad8a11a
André Fincato <work@andrefincato.info>
2023-01-25 19:05:28 +0100

    article: add authors tag
    blueprint + template. using a tag field for now, maybe each
    contributor should have their own page, so that we link to it both in
    blueprint and template.

--- site/snippets/grid/title.php
+++ site/snippets/grid/title.php
@@ -3,6 +3,13 @@
     <div class="block block-title" tabindex="0">
       <div class="contents">
         <h1><?= $page->title()->html() ?></h1>
+        <?php if ($authors = $page->authors()): ?>
+          <div class="article-authors">
+          <?php foreach ($authors->split(',') as $author): ?>
+            <span class="article-author"><?= $author ?></span>
+          <?php endforeach ?>
+          </div>
+        <?php endif ?>
       </div>
     </div>
   </div>
