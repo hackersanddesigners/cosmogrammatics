133cc25fde7d59d51e67cc858f575a8accc2be99
André Fincato <work@andrefincato.info>
2023-02-28 18:59:48 +0100

    remove previous footnote snipper system

--- site/snippets/footnotes/footnote.php
+++
@@ -1,8 +0,0 @@
-<?php $ref = $footnote->ref() ?>
-  <li id="note-ref-<?= $ref ?>">
-    <a
-      href="#ft-<?= $ref ?>"
-      class="note-ref-backlink"
-    >↩</a>
-    <?= $footnote->note() ?>
-  </li>
--- site/snippets/footnotes/index.php
+++
@@ -1,5 +0,0 @@
-  <ol class="notes column">
-    <?php foreach( $footnotes as $footnote ) {
-      snippet( 'footnotes/footnote', [ 'footnote' => $footnote ] );
-    } ?>
-  </ol>
