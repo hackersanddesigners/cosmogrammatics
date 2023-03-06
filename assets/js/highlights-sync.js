b94206071f81d7bbf117f6759d80d7c5c6fd41d9
André Fincato <work@andrefincato.info>
2023-03-06 18:59:03 +0100

    comments: correctly remove published comment from backend

--- assets/js/highlights-sync.js
+++ assets/js/highlights-sync.js
@@ -20,22 +20,38 @@ function textHighlightMergeUnique(newData, article_slug) {
   // w/ current localStorage['comment-{article-slug}']
   // filter out comments w/o block_id for backward support
 
-  // when removing comment from backend only, we need force
-  // remove it from frontend as well: how to?
+  // the problem is that upon publishing an article
+  // we're removing it from the store, but it's not really
+  // being removed (?);
+  // so when merging the two stores (backend and frontend)
+  // we're still adding the published articles that we should
+  // have removed. two ways:
+  // - fix the remove from store bug
+  // - filter out local store to keep only draft comments
 
   const comment_store = new LocalStore(`comment-${article_slug}`);
   let comments = comment_store.getAll();
+  const drafts = comments.filter(comment => comment.status === 'draft')
 
-  const highlightsMerged = [...comments, ...newData]
+  const highlightsMerged = [...drafts, ...newData]
   const highlights = highlightsMerged.filter((value, index, self) => {
     return index === self.findIndex(t => {
       return t.id === value.id && t.content.block_id !== null
     })
   })
 
-  // the save function checks if the item
-  // exists already, so we just pass
-  // the entire dataset
+  // to make sure we just set only the
+  // localStore draft comments together with
+  // the published comments from the backend:
+  // we remove and save the comment_store
+  // each time.
+  // this mostly solves the problem
+  // of removing a published comment from the
+  // backend without using other ways to
+  // pass that reference to the frontend and
+  // remove it from the localStore.
+  // brutal, but overall fine !:
+  comment_store.removeAll()
   comment_store.save(highlights)
 }
 
