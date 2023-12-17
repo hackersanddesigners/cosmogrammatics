5c1efdebc3f0ebef9be0e673025e1df9b6942feb
André Fincato <work@andrefincato.info>
2023-12-17 12:36:44 +0100

    article: diplay quote-block citation (author name, etc)

--- site/snippets/blocks/quote.php
+++ site/snippets/blocks/quote.php
@@ -1,10 +1,9 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
 <blockquote>
   <?= $block->text() ?>
-  <?php
-     if ($block->citation()->isNotEmpty()): ?>
-     <footer>
-       <a id="ft-<?= $block->id() ?>" href="#note-ref-<?= $block->id() ?>" class="ref-ft"><?= $block->citation() ?></a>
-     </footer>
-     <?php endif ?>
+  <?php if ($block->citation()->isNotEmpty()): ?>
+    <footer>
+      <a id="ft-<?= $block->id() ?>" href="#note-ref-<?= $block->id() ?>" class="ref-ft"><?= $block->citation() ?></a>
+    </footer>
+  <?php endif ?>
 </blockquote>
